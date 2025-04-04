e8c5655b5a3914d010d3eee15601d3ce74884696
SadN00dle <sadnoodle11@gmail.com>
2025-04-04 10:51:40 +0545

    add toggle function to the tab

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const skillBtn = document.getElementById("skillBtn").addEventListener("click", ()=>{
-    onClickShow("skillTab")
+    clear("skillTab");
+    onClickShow("skillTab");
+   // 
     
 })
 const expBtn = document.getElementById("expBtn").addEventListener("click", ()=>{
     onClickShow("expTab");
 })
 const eduBtn = document.getElementById("eduBtn").addEventListener("click", ()=>{
+    clear("eduTab");
     onClickShow("eduTab");
+    //
+
 })
 
 
+function clear(currentTab){
+    const tabs = ["skillTab", "expTab", "eduTab"]
+    tabs.forEach((tabId) => {
+        const tab = document.getElementById(tabId);
+        if(tabId !== currentTab){
+            tab.style.display = 'none';
+        }
+    });
+}
 
 function onClickShow(tabName){
     const hiddenText = document.getElementById(tabName);
@@ -18,9 +32,7 @@ function onClickShow(tabName){
         hiddenText.style.display = "block";
     }else{
         hiddenText.style.display = "none";
-    }
-    
-    
+    } 
 }
 
 
