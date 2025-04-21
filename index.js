20a928ec427ef867a100cc29b1444c6132d4666f
SadN00dle <sadnoodle11@gmail.com>
2025-04-21 22:55:29 +0545

    styles folder to arrange styles, add service style and styled plus HTML

--- index.js
+++ index.js
@@ -7,32 +7,36 @@ let expTab = document.getElementById("expTab")
 let eduTab = document.getElementById("eduTab")
 
 let tabContent = document.querySelectorAll(".tabContent")
+let tabLinks = document.querySelectorAll(".tabLinks")
 
 //adding click function to buttons
 skillBtn.addEventListener("click", ()=>{
-    clickShow(skillTab)
+    clickShow(skillTab, skillBtn)
 })
 expBtn.addEventListener("click", ()=>{
 
-   clickShow(expTab);
+   clickShow(expTab, expBtn);
 })
 eduBtn.addEventListener("click", ()=>{
-    clickShow(eduTab);
+    clickShow(eduTab, eduBtn);
 })
 
 //click function to add active state to tab class
-function clickShow(tab){
+function clickShow(tab, btn){
     //deactivate all the buttons first
     if (tab.classList.contains('active')){
         tab.classList.remove('active')
+        btn.classList.remove('active')
     }else{
         tabContent.forEach((content) =>{
             content.classList.remove('active');
         })
+        tabLinks.forEach((link) =>{
+            link.classList.remove('active')
+        })
         
         //activate afterwards to only activate the selected button 
-        tab.classList.add('active');
+        tab.classList.add('active')
+        btn.classList.add('active')
     }
-   
-
 }
